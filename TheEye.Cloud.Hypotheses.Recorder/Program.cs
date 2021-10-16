33f1bb9d3ad443aaba8ae682ccf23295511686ca
EdvardXYZ <eddumansky@gmail.com>
2021-10-16 13:18:51 +0300

    Replace space indents with tabs

--- TheEye.Cloud.Hypotheses.Recorder/Program.cs
+++ TheEye.Cloud.Hypotheses.Recorder/Program.cs
@@ -4,23 +4,23 @@ using Microsoft.Extensions.Hosting;
 
 namespace TheEye.Cloud.Hypotheses.Recorder
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            var url = "http://0.0.0.0:" + port;
+		public static IHostBuilder CreateHostBuilder(string[] args)
+		{
+			var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+			var url = "http://0.0.0.0:" + port;
 
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().UseUrls(url);
-                });
-        }
-    }
+			return Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>().UseUrls(url);
+				});
+		}
+	}
 }
